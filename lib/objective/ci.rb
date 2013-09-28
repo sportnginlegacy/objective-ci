725faac1c01aa7183b2d93b128f0589b1add9541
Mark Larsen <larse503@gmail.com>
2013-09-28 10:05:33 -0500

    initial commit

---
+++ lib/objective/ci.rb
@@ -0,0 +1,7 @@
+require "objective/ci/version"
+
+module Objective
+  module Ci
+    # Your code goes here...
+  end
+end
---
+++ lib/objective/ci/version.rb
@@ -0,0 +1,5 @@
+module Objective
+  module Ci
+    VERSION = "0.0.1"
+  end
+end
